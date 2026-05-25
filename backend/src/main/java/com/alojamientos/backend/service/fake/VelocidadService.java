915ea033c8e8da2dff9c672ef1a4e79521921b8f
AinesLaRealG <2inesiita2@gmail.com>
2026-05-25 20:43:40 +0200

    Update VelocidadService.java

--- backend/src/main/java/com/alojamientos/backend/service/fake/VelocidadService.java
+++ backend/src/main/java/com/alojamientos/backend/service/fake/VelocidadService.java
@@ -6,6 +6,11 @@ import java.util.Map;
 @Service
 public class VelocidadService {
     public Map<String, Integer> getEventosRecientes(String alojamientoId) {
+        if (alojamientoId.equals("1")) {
         return Map.of("visitas_recientes", 40, "likes_recientes", 12);
+    } else if (alojamientoId.equals("2")) {
+        return Map.of("visitas_recientes", 15, "likes_recientes", 5);
+    }
+    return Map.of("visitas_recientes", 0, "likes_recientes", 0);
     }
 }
