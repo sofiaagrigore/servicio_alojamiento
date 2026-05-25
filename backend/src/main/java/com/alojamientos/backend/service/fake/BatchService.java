0aed7d264dd2a9dfd8f5b8e225da334ed542b001
AinesLaRealG <2inesiita2@gmail.com>
2026-05-25 20:43:16 +0200

    Update BatchService.java

--- backend/src/main/java/com/alojamientos/backend/service/fake/BatchService.java
+++ backend/src/main/java/com/alojamientos/backend/service/fake/BatchService.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class BatchService {
     public Map<String, Integer> getDatosHistoricos(String alojamientoId) {
+        if (alojamientoId.equals("1")) {
         return Map.of("visitas_historicas", 1500, "likes_historicos", 330);
+    } else if (alojamientoId.equals("2")) {
+        return Map.of("visitas_historicas", 800, "likes_historicos", 150);
+    }
+    return Map.of("visitas_historicas", 0, "likes_historicos", 0);
     }
 }
